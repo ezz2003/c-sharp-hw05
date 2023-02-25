c0a99f71205aeab0501c5c8e09326a6538c37f10
ezz2003 <ezz2003@yandex.ru>
2023-02-25 19:06:59 +1000

    solving the problem35

---
+++ problem35/Program.cs
@@ -0,0 +1,30 @@
+﻿// 35. задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]
+
+int[] MakeArrRnd(int size, int min, int max)
+{
+  int[] array = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < size; i++) array[i] = rnd.Next(min, max + 1);
+  return array;
+}
+
+string MakeStrPrintArr(int[] arr)
+{
+  string strPrn = string.Empty;
+  for (int i = 0; i < arr.Length - 1; i++) strPrn += $"{arr[i]}, ";
+  strPrn += arr.Last();
+  return $"[ {strPrn} ]";
+}
+
+int CountNumbers(int[] ar)
+{
+  int count = 0;
+  for (int i = 0; i < ar.Length; i++)
+  {
+    if (ar[i] >= 10 & ar[i] < 100) count++;
+  }
+  return count;
+}
+
+int[] readyArr = MakeArrRnd(123, 1, 999);
+Console.WriteLine($"{MakeStrPrintArr(readyArr)} -> {CountNumbers(readyArr)}");
