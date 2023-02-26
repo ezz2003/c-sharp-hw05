ee792c38d4e7850faf4dd9af71f487bb4b4376b5
ezz2003 <ezz2003@yandex.ru>
2023-02-27 08:45:35 +1000

    solving the problem34

---
+++ problem34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// 
+// [345, 897, 568, 234] -> 2
+// 
+
+int[] MakeArrRnd(int size, int min, int max)
+{
+  int[] array = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < size; i++) array[i] = rnd.Next(min, max + 1);
+  return array;
+}
+
+string MakeStrPrintArr(int[] arr)
+{
+  string strPrn = string.Empty;
+  for (int i = 0; i < arr.Length - 1; i++) strPrn += $"{arr[i]}, ";
+  strPrn += arr.Last();
+  return $"[ {strPrn} ]";
+}
+
+int CountEvenNumbers(int[] ar)
+{
+  int count = 0;
+  for (int i = 0; i < ar.Length; i++)
+  {
+    if (ar[i] % 2 == 0) count++;
+  }
+  return count;
+}
+
+int[] readyArr = MakeArrRnd(12, 100, 1000);
+Console.WriteLine($"{MakeStrPrintArr(readyArr)} -> {CountEvenNumbers(readyArr)}");
